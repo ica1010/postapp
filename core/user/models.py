1b4a219119b938b5f53e8678c4568a3622d6fcb7
ica1010 <ghislinametonou@gmail.com>
2024-07-16 14:12:05 +0000

    ****

--- core/user/models.py
+++ core/user/models.py
@@ -9,6 +9,17 @@ class ProfileEmployeur(models.Model):
     eid = ShortUUIDField(unique=True, max_length=255, length= 20 , primary_key=True , editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
     active = models.BooleanField(default=True)
+    phone = models.CharField(max_length=50, default='')
+
+    facebook_link =  models.URLField(max_length=250, blank=True, null=True, default='')
+    instagram_link =  models.URLField(max_length=250, blank=True, null=True, default='')
+    twetter_link =  models.URLField(max_length=250, blank=True, null=True, default='')
+    youtube_link =  models.URLField(max_length=250, blank=True, null=True, default='')
+    linkedin_link =  models.URLField(max_length=250, blank=True, null=True, default='')
+
+    mail = models.EmailField(max_length=150, default='' , blank=True, null=True)
+    phone = models.CharField(max_length=150, default='' , blank=True, null=True)
+    whatsapp = models.CharField(max_length=150, default='' , blank=True, null=True)
 
     def __str__(self):
         return self.user.username 
